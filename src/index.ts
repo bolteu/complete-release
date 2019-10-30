0a7363b4d98ba7916a9da2d3c905abd1a53f867f
Pavlo Pakholka <pavlo.pakholka@bolt.eu>
2019-10-30 15:44:41 +0200

    Test exec context

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import * as exec from '@actions/exec';
 import fetch from 'node-fetch';
 
 async function run() {
@@ -20,6 +21,24 @@ async function run() {
     const pullRequest = await fetch(pullRequestUrl).then(data => data.json())
 
     console.log(`PR payload: \n ${JSON.stringify(pullRequest)}!`);
+
+    const headRef:string = pullRequest.head.ref;
+    const baseRef:string = pullRequest.base.ref;
+
+    let myOutput = '';
+    let myError = '';
+
+    const options:any = {};
+    options.listeners = {
+      stdout: (data: Buffer) => {
+        myOutput += data.toString();
+      },
+      stderr: (data: Buffer) => {
+        myError += data.toString();
+      }
+    };
+    await exec.exec('git', ['config', 'user.name'], options);
+
 
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
